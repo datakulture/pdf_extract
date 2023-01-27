d5ec0bd41b2544fd037881af7a6c52a84829b626
DataKulture <115214447+datakulture@users.noreply.github.com>
2023-01-27 14:52:11 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -50,7 +50,9 @@ def displayPDF(file):
   base64_pdf = base64.b64encode(file).decode('utf-8')
 
   # Embedding PDF in HTML
-  pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+  #pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+  pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">' 
+
   # Displaying File
   st.markdown(pdf_display, unsafe_allow_html=True)
 
